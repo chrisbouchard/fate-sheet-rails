0c42943f4f15250fcc6cc292ad9e47577437f2df
Chris Bouchard <chris@upliftinglemma.net>
2021-06-13 01:29:48 -0400

    Configure paging for collections

--- config/initializers/jsonapi-resources.rb
+++ config/initializers/jsonapi-resources.rb
@@ -3,6 +3,10 @@
 JSONAPI.configure do |config|
   config.resource_cache = Rails.cache
 
+  config.default_paginator = :paged
+  config.top_level_meta_include_record_count = true
+  config.top_level_meta_include_page_count = true
+
   # TODO: These lines can be enabled once we upgrade to 0.10.
   # config.include_backtraces_in_errors = false
   # config.include_application_backtraces_in_errors = false
