16234c98b2a35dfd2fb0511fd9634f80670e62e7
Chris Bouchard <chris@upliftinglemma.net>
2021-07-03 01:44:42 -0400

    Remove direct routes for aspects and skills
    They can only be authorized in the context of a character.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  jsonapi_resources :aspects
   jsonapi_resources :characters
-  jsonapi_resources :memberships
-  jsonapi_resources :skills
   jsonapi_resources :users
   jsonapi_resources :worlds
 end
