213c36fffb36436c4a9ad326bf6c05bb33facbd8
Chris Bouchard <chris@upliftinglemma.net>
2021-07-31 00:30:09 -0400

    Route to all resource types now that scopes are correct
    Since all resource types will be scoped to the current user if
    applicable, we don't need to limit routable resources anymore. This will
    also resolve the warnings about links not being generated, and will
    eventually allow resources to be updated.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  jsonapi_resources :aspects
   jsonapi_resources :characters
+  jsonapi_resources :skills
+  jsonapi_resources :stress_boxes
+  jsonapi_resources :stress_tracks
   jsonapi_resources :users
   jsonapi_resources :worlds
 end
