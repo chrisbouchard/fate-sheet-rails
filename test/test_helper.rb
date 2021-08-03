74edac91666f9fcf839359dd5e5bfa70d702ae87
Chris Bouchard <chris@upliftinglemma.net>
2021-08-03 01:16:07 -0400

    Add controller test helpers to mock Auth0
    I don't want to pass around actual JWT tokens for most of these
    controller tests, so these helpers will inject a mock authenticator that
    accepts any token and returns a test-specified auth0_id.
    
    The auth0_headers helper is mostly for convenience. We could pass any
    token, but we have to pass *some* token, so it fixes a fake value and
    builds the Authorization header.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'minitest/autorun'
 
 module ActiveSupport
   class TestCase
@@ -13,5 +14,35 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+  end
+end
+
+class ResourceControllerTest < ActionDispatch::IntegrationTest
+  FAKE_AUTH0_TOKEN = 'fake.auth0.token'
+
+  setup do
+    ActionDispatch::IntegrationTest.register_encoder :api_json,
+      param_encoder: ->(params) { params.to_json },
+      response_parser: ->(body) { JSON.parse(body) }
+  end
+
+  # Create a mock for the Auth0 authenticator and stub it.
+  def mock_auth0(auth0_id_or_user)
+    authenticator = Minitest::Mock.new
+
+    if auth0_id_or_user.respond_to? :auth0_id
+      authenticator.expect :auth0_id, auth0_id_or_user.auth0_id
+    elsif !auth0_id_or_user.nil?
+      authenticator.expect :auth0_id, auth0_id_or_user
+    end
+
+    Auth0Authenticator.stub :new, authenticator do
+      yield authenticator
+    end
+  end
+
+  # Set headers required by the API.
+  def auth0_headers(token: FAKE_AUTH0_TOKEN)
+    { 'Authorization': "Bearer #{token}" }
   end
 end
