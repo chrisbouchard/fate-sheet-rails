10468a7e16e45d46dab1d7cb0724bd650943b336
Chris Bouchard <chris@upliftinglemma.net>
2022-05-01 00:19:16 -0400

    Reformat associations to be multiline
    These associations are complex enough that multiple lines are helpful.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class Character < ApplicationRecord
-  has_many :aspects, as: :aspected, dependent: :destroy, inverse_of: :aspected
-  has_many :skills, dependent: :destroy
-  has_many :stress_tracks, dependent: :destroy
+  has_many :aspects,
+    as: :aspected,
+    dependent: :destroy,
+    inverse_of: :aspected
+  has_many :skills,
+    dependent: :destroy
+  has_many :stress_tracks,
+    dependent: :destroy
 
   belongs_to :world
 
--- app/models/world.rb
+++ app/models/world.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 class World < ApplicationRecord
-  has_many :aspects, as: :aspected, dependent: :destroy, inverse_of: :aspected
+  has_many :aspects,
+    as: :aspected,
+    dependent: :destroy,
+    inverse_of: :aspected
   has_many :characters
 
-  has_many :memberships, dependent: :destroy
-  has_many :users, through: :memberships, inverse_of: :worlds
+  has_many :memberships,
+    dependent: :destroy
+  has_many :users,
+    through: :memberships,
+    inverse_of: :worlds
 
   validates :name, presence: true
 
