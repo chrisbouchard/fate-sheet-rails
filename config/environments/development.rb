83cc590340da1eda3436408326b897f51d179236
Chris Bouchard <chris@upliftinglemma.net>
2022-04-30 22:38:17 -0400

    Undo changes to log format
    I changed my mind on this one. The built-in formatter is good. It includes the
    timestamp at the start of the request entry.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,8 +64,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
   config.log_level = :debug
 end
