7bb2472fcdd46bab2fa7946d83687a8bbda7640d
Chris Bouchard <chris@upliftinglemma.net>
2022-05-22 01:00:18 -0400

    Expose a character's portrait on its resource

--- app/resources/character_resource.rb
+++ app/resources/character_resource.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class CharacterResource < BaseResource
+  include HasAttachment
+
   attributes :name, :fate_points, :refresh
+
+  has_one_attached :portrait, variants: [:sheet, :thumbnail]
+
   has_many :aspects
   has_many :skills
   has_many :stress_tracks
