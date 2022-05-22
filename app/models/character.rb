97152bfdfc9dacf92c6e690ea8e2008ec615317f
Chris Bouchard <chris@upliftinglemma.net>
2022-05-22 00:59:53 -0400

    Add a portrait image attached to characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -12,6 +12,11 @@ class Character < ApplicationRecord
 
   belongs_to :world
 
+  has_one_attached :portrait do |attachable|
+    attachable.variant :sheet, resize_to_limit: [400, 800]
+    attachable.variant :thumbnail, resize_to_fill: [400, 400, { crop: :attention }]
+  end
+
   validates :name, presence: true
   validates :fate_points, :power_level, :refresh, :skill_points,
     numericality: { greater_than_or_equal_to: 0, only_integer: true },
