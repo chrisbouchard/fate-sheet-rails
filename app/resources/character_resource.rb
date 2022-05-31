d4118ca37a251e2d16db6be5e01289ba1a62e8f5
Chris Bouchard <chris@upliftinglemma.net>
2022-05-30 23:03:20 -0400

    Switch character resource to use portrait resource
    Rather than exposing its own blob/variant links. This will allow us to skip
    loading the attachments if they're not necessary, and will work better with
    models that have multiple attachments -- both multiple different attachments (to
    avoid the number of links growing large) and also has_many_attached attachments
    that have multiple files (which were not handled at all before).

--- app/resources/character_resource.rb
+++ app/resources/character_resource.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
 class CharacterResource < BaseResource
-  include HasAttachment
-
   attributes :name, :fate_points, :refresh
-
-  has_one_attached :portrait, variants: [:sheet, :thumbnail]
 
   has_many :aspects
   has_many :skills
   has_many :stress_tracks
   has_one :world
+
+  has_one :portrait, relation_name: :portrait_attachment
 
   filter :name
 end
