755f079eb66ae0ddde5eb4f5e45dea25c5de94d9
Chris Bouchard <chris@upliftinglemma.net>
2022-07-03 21:55:33 -0400

    Remove unnecessary JSONAPI::Resources configuration

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -15,12 +15,5 @@ JSONAPI.configure do |config|
   config.top_level_meta_include_record_count = true
   config.top_level_meta_include_page_count = true
 
-  # TODO: These lines can be enabled once we upgrade to 0.10.
-  # config.include_backtraces_in_errors = false
-  # config.include_application_backtraces_in_errors = false
-
-  config.always_include_to_one_linkage_data = true
-  config.always_include_to_many_linkage_data = true
-
   config.use_relationship_reflection = true
 end
